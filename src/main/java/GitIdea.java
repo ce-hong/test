5427d291feb1daff6695a3d2b7f78cef53546904
ce_glb <ce_glb@123.com>
2022-04-24 15:43:24 +0800

    更新3

--- src/main/java/GitIdea.java
+++ src/main/java/GitIdea.java
@@ -3,5 +3,6 @@ public class GitIdea {
     public static void main(String[] args) {
         System.out.println("Git_Integrate_Idea");
         System.out.println("更新2");
+        System.out.println("更新3");
     }
 }
